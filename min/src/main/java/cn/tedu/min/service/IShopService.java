2fe5bb13c39ee6894016debf6f203a345aee308d
mr <2293683246@qq.com>
2022-08-09 09:43:39 +0800

    %6

--- min/src/main/java/cn/tedu/min/service/IShopService.java
+++ min/src/main/java/cn/tedu/min/service/IShopService.java
@@ -4,7 +4,7 @@ import cn.tedu.min.common.entity.dto.AddShopDTO;
 import cn.tedu.min.common.entity.dto.ShopDTO;
 import cn.tedu.min.common.entity.dto.UpdateShopDateilDTO;
 import cn.tedu.min.common.entity.vo.ShopDetailVO;
-//vnksldn
+//vnksldnbuibv
 public interface IShopService {
 
     void   addShop(AddShopDTO addShopDTO);
